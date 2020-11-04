2868bc07c7037c9700d2e5fdb967aba4869a277c
unknown <fdealmeida@inflexsys.com>
2020-11-04 23:39:06 +0100

    remove imports

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 let schedule = require('node-schedule');
-import { DomainParserService } from './services/domain-parser.service';
 import { DomainRetrieverService } from './services/domain-retriever.service';
 
 export class App {
     private domainRetrieverService: DomainRetrieverService;
-    private domainParserService: DomainParserService;
 
     constructor() {
         this.domainRetrieverService = new DomainRetrieverService();
-        this.domainParserService = new DomainParserService();
     }
 
     public async init() {
